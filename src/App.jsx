ae435bb42133b3fab7d7af63090b974f686b7282
vvvvvv eeeeee <hiddenether@yahoo.com>
2025-09-14 05:25:55 -0400

    added "add to cart" functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
 import './App.css'
 import product1 from './images/camera lens.jpg'
 import product2 from './images/camera.jpg'
@@ -8,14 +9,23 @@ import product5 from './images/sd card.jpg'
 
 
 function App() {
+  const [cart, setCart] = useState(getDefaultCart());
+
+  function getDefaultCart() {
+    let ca = {}
+    for(let product of PRODUCTS)
+      ca[product.id] = 0;
+
+    return ca;
+  }
   
   return (
     <>
       <Router>
         <Navbar></Navbar>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
+          <Route path='/' element={<Home cart={cart} setCart={setCart} />} />
+          <Route path='/cart' element={<Cart cart={cart} />} />
         </Routes>
       </Router>
     </>
@@ -31,41 +41,66 @@ function Navbar() {
   );
 }
 
-function Home() {
+function Home({ cart, setCart }) {
   return (
     <>
       <h1>Home</h1>
-      <Shop />
+      <Shop cart={cart} setCart={setCart} />
     </>
   );
 }
 
-function Cart() {
+function Cart({ cart }) {
   return (
     <>
-      <h1>Cart</h1>
-      <p>This is your cart</p>
+      <h1>Your Cart</h1>
+      <div className="customer-cart">
+        {PRODUCTS.filter(product => cart[product.id] > 0).map(product => (
+          <CartItem key={product.name} data={product} qty={cart[product.id]} />
+        ))}
+      </div>
     </>
   );
 }
 
-function Product(props) {
-  const { id, name, file, price } = props.data;
+function CartItem( { data, qty }) {
+  const { id, name, file, price } = data;
 
   return (
     <div className="product">
       <img src={file} />
       <h2>{name}</h2>
       <p>${price}</p>
+      <p>Qty: {qty}</p>
     </div>
   );
 }
 
-function Shop() {
+function Product({ data, cart, setCart }) {
+  const { id, name, file, price } = data;
+
+  function handleAddToCart(id) {
+    setCart({
+      ...cart,
+      [id]: cart[id] + 1,
+    })
+  }
+
+  return (
+    <div className="product">
+      <img src={file} />
+      <h2>{name}</h2>
+      <p>${price}</p>
+      <button className="add-cart" onClick={() => handleAddToCart(id)} >Add to Cart</button>
+    </div>
+  );
+}
+
+function Shop({ cart, setCart }) {
   return (
     <div className="shop">
       {PRODUCTS.map((product) => (
-        <Product key={product.id} data={product} />
+        <Product data={product} cart={cart} setCart={setCart} key={product.name}/>
       ))}
     </div>
   );
